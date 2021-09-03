7328a08b08de5b370bd2cb4ee5da70b8b80deb2e
Federico Luzzi <fedeluzzi00@gmail.com>
2021-09-03 22:20:39 +0200

    feat: add rinkeby support

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,9 @@ require("@nomiclabs/hardhat-truffle5");
 require("./tasks/deploy");
 require("./tasks/check-deployment");
 
+const infuraId = process.env.INFURA_ID;
+const accounts = process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [];
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
@@ -10,32 +13,36 @@ module.exports = {
     networks: {
         arbitrumTestnetV5: {
             url: "https://kovan5.arbitrum.io/rpc",
-            accounts: [process.env.PRIVATE_KEY],
+            accounts,
             gasPrice: 0,
             gas: 1000000000,
             timeout: 100000,
         },
         arbitrumMainnet: {
             url: "https://arb1.arbitrum.io/rpc",
-            accounts: [process.env.PRIVATE_KEY],
+            accounts,
             gasPrice: 0,
             timeout: 100000,
         },
+        rinkeby: {
+            url: `https://rinkeby.infura.io/v3/${infuraId}`,
+            accounts,
+        },
         arbitrumRinkebyTestnet: {
             url: "https://rinkeby.arbitrum.io/rpc",
-            accounts: [process.env.PRIVATE_KEY],
+            accounts,
             gasPrice: 0,
             timeout: 100000,
         },
         sokol: {
             url: "https://sokol.poa.network",
-            accounts: [process.env.PRIVATE_KEY],
+            accounts,
             gasPrice: 1000000000,
             timeout: 100000,
         },
         xdai: {
             url: "https://rpc.xdaichain.com/",
-            accounts: [process.env.PRIVATE_KEY],
+            accounts,
             gasPrice: 1000000000,
             timeout: 100000,
         },
